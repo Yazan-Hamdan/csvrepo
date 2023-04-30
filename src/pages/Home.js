611d1114b9f9b0d5fee8cd95a698311edaff858e
yazanhamdan591 <yazanalatrash591@gmail.com>
2023-04-30 16:45:46 +0300

    added multiusers level

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,22 @@ const Home = () =>{
         {({ signOut, user }) => (
           <main>
             <h1>Hello {user.username}</h1>
+            {user && user.signInUserSession && user.signInUserSession.accessToken && (
+              <div>
+                {user.signInUserSession.accessToken.payload['cognito:groups'] &&
+                  user.signInUserSession.accessToken.payload['cognito:groups'].includes('Admins') && (
+                    <p>You are an Admin user</p>
+                )}
+                {user.signInUserSession.accessToken.payload['cognito:groups'] &&
+                  user.signInUserSession.accessToken.payload['cognito:groups'].includes('Writers') && (
+                    <p>You are a Writer user</p>
+                )}
+                {user.signInUserSession.accessToken.payload['cognito:groups'] &&
+                  user.signInUserSession.accessToken.payload['cognito:groups'].includes('Readers') && (
+                    <p>You are a Reader user</p>
+                )}
+              </div>
+            )}
             <button onClick={signOut} className='amazon-button'>Sign out</button>
           </main>
         )}
