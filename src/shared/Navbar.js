bec09ac91dba8b87eacd0e1b02091660e3c708e6
yazanhamdan591 <yazanalatrash591@gmail.com>
2023-05-03 12:53:18 +0300

    added a button to redirect the admin to user pool management

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './Navbar.css';
 
+
 const Navbar = (props) => {
-    return (
-      <nav className='navbar'>
-        <div className='navbar-group'>
-          <p>You have {props.userGroup} roles</p>
-        </div>
+  const cognitoURL = process.env.REACT_APP_COGINTO_URL
+  return (
+    <nav className='navbar'>
+      <div className='navbar-group'>
+        <p>You have {props.userGroup} roles</p>
+      </div>
+      <div className='button-group'>
+      {props.userGroup == 'Admins' && 
+          <div className='button-group'>
+            <button onClick={() => window.open(cognitoURL)} className="amazon-button">
+              Manage Groups
+            </button>
+          </div>
+        }
         <button onClick={props.SignOutHandler} className="amazon-button">
           Sign out
-       </button>
-      </nav>
-    );
-  };
+        </button>
+      </div>
+    </nav>
+  );
+};
   
-  export default Navbar;
+export default Navbar;
